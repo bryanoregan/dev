2c3eb1a7236c2a8b66ff0d360dbbc2633848b010
bryanoregan <bryanoregan@gmail.com>
2014-07-25 00:48:30 +0100

    first app just before adding action bar

--- secondApp/src/com/example/secondapp/MainActivity.java
+++ secondApp/src/com/example/secondapp/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.secondapp;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
-    @Override
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+	    Intent intent = new Intent(this, DisplayMessageActivity.class);
+	    EditText editText = (EditText) findViewById(R.id.edit_message);
+	    String message = editText.getText().toString();
+	    intent.putExtra(EXTRA_MESSAGE, message);
+	    startActivity(intent);
+	}
+	
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
